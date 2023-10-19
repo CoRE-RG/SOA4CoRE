6667b97f6667a4a2fa20d619ac49aa527ab35798
Timo Haeckel <timo.haeckel@haw-hamburg.de>
2023-10-19 17:09:10 +0200

    add base functionality to create ip filters to add 802.1q-tags to ip traffic of publishers

--- src/soa4core/endpoints/publisher/standard/base/STDPublisherEndpointBase.cc
+++ src/soa4core/endpoints/publisher/standard/base/STDPublisherEndpointBase.cc
@@ -17,11 +17,38 @@
 
 #include "STDPublisherEndpointBase.h"
 
+#include "core4inet/utilities/ConfigFunctions.h"
+#include "core4inet/utilities/ModuleAccess.h"
+#include "core4inet/buffer/base/BGBuffer.h"
+
+using namespace std;
+using namespace inet;
+using namespace CoRE4INET;
 
 namespace SOA4CoRE {
 
+void STDPublisherEndpointBase::initialize()
+{
+    PublisherEndpointBase::initialize();
 
-void STDPublisherEndpointBase::handleParameterChange(const char* parname) {
+    cModule* networkLayer = findModuleWhereverInNode("networkLayer", this);
+    if(!networkLayer)
+    {
+        throw cRuntimeError("Network Layer could not be resolved during initialization.");
+    }
+    cModule* ipModule = networkLayer->getSubmodule("ip");
+    if(!ipModule)
+    {
+        throw cRuntimeError("Ip module could not be resolved during initialization.");
+    }
+    if(!(this->_networkLayer = dynamic_cast<IPv4oREBase*>(ipModule)))
+    {
+        throw cRuntimeError("Ip module is not of type IPv4oREBase.");
+    }
+}
+
+void STDPublisherEndpointBase::handleParameterChange(const char* parname)
+{
     PublisherEndpointBase::handleParameterChange(parname);
 
     if (!parname || !strcmp(parname, "localAddress"))
@@ -32,6 +59,110 @@ void STDPublisherEndpointBase::handleParameterChange(const char* parname) {
     {
         _localPort = par("localPort");
     }
+    if (!parname || !strcmp(parname, "vlan_id"))
+    {
+        if(par("vlan_id").intValue() != -1)
+        {
+            this->_vlanID = static_cast<uint16_t>(parameterULongCheckRange(par("vlan_id"), 0, MAX_VLAN_ID));
+        }
+    }
+    if (!parname || !strcmp(parname, "pcp"))
+    {
+        if(par("pcp").intValue() != -1)
+        {
+            this->_pcp = static_cast<uint8_t>(parameterULongCheckRange(par("pcp"), 0, MAX_Q_PRIORITY));
+        }
+    }
+    if (!parname || !strcmp(parname, "localAddress"))
+    {
+        _localAddress = par("localAddress").stdstringValue();
+    }
+}
+
+void STDPublisherEndpointBase::createAndInstallFilter(inet::IPv4Address destAddr, int srcPort, int destPort)
+{
+    MACAddress macAddress = resolveDestMacAddress(destAddr);
+    TrafficPattern* tp = createTrafficPattern(destAddr, srcPort, destPort);
+    IEEE8021QDestinationInfo *destInfo = createDestinationInfo(macAddress);
+    IPoREFilter *filter = new IPoREFilter();
+    filter->setDestInfo(destInfo);
+    filter->setTrafficPattern(tp);
+    installFilter(filter);
+}
+
+void STDPublisherEndpointBase::installFilter(IPoREFilter* filter)
+{
+    _networkLayer->addFilter(filter);
+}
+
+TrafficPattern* STDPublisherEndpointBase::createTrafficPattern(
+        inet::IPv4Address destAddr, int srcPort, int destPort)
+{
+    TrafficPattern* tp = new TrafficPattern();
+    tp->setDestAddr(destAddr);
+    tp->setDestPrefixLength(32);
+    tp->setSrcPortMin(srcPort);
+    tp->setSrcPortMax(srcPort);
+    tp->setDestPortMin(destPort);
+    tp->setDestPortMax(destPort);
+    return tp;
+}
+
+IEEE8021QDestinationInfo* STDPublisherEndpointBase::createDestinationInfo(MACAddress destMAC)
+{
+    list<cGate*> destGates;
+    // use default gate bg out as in IPv4oIEEE8021Q
+    cModule* module = findModuleWhereverInNode(_defaultDestGate.c_str(), this);
+    if (!module)
+    {
+        throw cRuntimeError("Default destModule \"%s\" could not be resolved.", _defaultDestGate.c_str());
+    }
+    if (BGBuffer *bgBuf = dynamic_cast<BGBuffer*>(module))
+    {
+        destGates.push_back(bgBuf->gate("in"));
+    } else
+    {
+        throw cRuntimeError("destModule: %s is not a BGBuffer and destGate could not be resolved.", _defaultDestGate.c_str());
+    }
+    return createDestinationInfo(_vlanID, _pcp, destMAC, destGates);
+}
+
+IEEE8021QDestinationInfo* STDPublisherEndpointBase::createDestinationInfo(uint16_t vid, uint8_t pcp, MACAddress destMAC,
+        std::list<cGate*>& destGates)
+{
+    IEEE8021QDestinationInfo *destInfo = new IEEE8021QDestinationInfo();
+    destInfo->setDestType(DestinationType_8021Q);
+    destInfo->setVID(vid);
+    destInfo->setPCP(pcp);
+    destInfo->setDestMac(new MACAddress(destMAC));
+    destInfo->setDestGates(destGates);
+    return destInfo;
+}
+
+MACAddress STDPublisherEndpointBase::resolveDestMacAddress(inet::IPv4Address destAddress)
+{
+    MACAddress destMac;
+    if (destAddress.isLimitedBroadcastAddress())
+    {
+        destMac = MACAddress::BROADCAST_ADDRESS;
+    }
+    else if (destAddress.isMulticast())
+    {
+        destMac = MACAddress::makeMulticastAddress(destAddress);
+    }
+    else
+    {
+        if(_arp == nullptr) {
+            if(!(_arp = dynamic_cast<IARP*>(findModuleWhereverInNode("arp", this)))) {
+                throw cRuntimeError("MAC Address could not be resolved because arp module was'nt found.");
+            }
+        }
+        destMac = _arp->resolveL3Address(destAddress, nullptr);
+        if(destMac.isUnspecified()) {
+            throw cRuntimeError("Could not resolve MAC address with ARP directly. RT IP stack does not support dynamic discovery yet, maybe try GlobalARP.");
+        }
+    }
+    return destMac;
 }
 
 } /*end namespace SOA4CoRE*/
--- src/soa4core/endpoints/publisher/standard/base/STDPublisherEndpointBase.h
+++ src/soa4core/endpoints/publisher/standard/base/STDPublisherEndpointBase.h
@@ -20,6 +20,15 @@
 
 #include "soa4core/endpoints/publisher/base/PublisherEndpointBase.h"
 
+#include "core4inet/networklayer/inet/base/IPv4oREBase.h"
+#include "core4inet/networklayer/inet/base/IPoREFilter.h"
+#include "core4inet/networklayer/inet/IEEE8021Q/IEEE8021QDestinationInfo.h"
+#include "core4inet/networklayer/inet/base/TrafficPattern.h"
+#include "inet/linklayer/common/MACAddress.h"
+#include "inet/networklayer/contract/ipv4/IPv4Address.h"
+#include "inet/networklayer/contract/IARP.h"
+#include <list>
+
 namespace SOA4CoRE {
 
 /**
@@ -28,6 +37,7 @@ namespace SOA4CoRE {
  * during runtime to connect realise a publishing endpoint for a service.
  * Publishers are connected to a specific transport and a publisher connector module (@see~PublisherConnector)
  * which loosely couples the publisher endpoint with a publisher application (@see~Publisher).
+ * Optionally allows to configure the IPv4oRE stack from CoRE4INET to add 802.1Q tags.
  *
  * @ingroup soa4core/endpoints
  *
@@ -40,7 +50,60 @@ class STDPublisherEndpointBase : public PublisherEndpointBase
  */
 public:
 protected:
+    virtual void initialize() override;
     virtual void handleParameterChange(const char* parname) override;
+    CoRE4INET::TrafficPattern * createTrafficPattern() {
+        return new CoRE4INET::TrafficPattern();
+    }
+
+    /**
+     * Creates and installs a traffic filter on the IPv4oIEEE8021Q network layer
+     * @param destAddr the destination IP
+     * @param srcPort the transport src port
+     * @param destPort the transport dest port
+     */
+    void createAndInstallFilter(inet::IPv4Address destAddr, int srcPort, int destPort);
+
+    /**
+     * Installs the given filter on the IPv4oIEEE8021Q network layer
+     * @param filter the filter to be added
+     */
+    void installFilter(CoRE4INET::IPoREFilter* filter);
+
+    /**
+     * Build a traffic pattern based on the destIP, srcPort and destPort.
+     * @param destAddr the destination IP
+     * @param srcPort the transport src port
+     * @param destPort the transport dest port
+     * @return a traffic pattern for streams from this endpoint
+     */
+    CoRE4INET::TrafficPattern* createTrafficPattern(inet::IPv4Address destAddr, int srcPort, int destPort);
+
+    /**
+     * Builds a 802.1Q destination info used in a IPoREFilter to create the QHeader on layer 2
+     * Calls the createDestinationInfo with the class attributes and resolves the default gate
+     * @param destMAC the destination MAC to be used
+     * @return the destination info
+     */
+    CoRE4INET::IEEE8021QDestinationInfo* createDestinationInfo(inet::MACAddress destMAC);
+
+    /**
+     * Builds a 802.1Q destination info used in a IPoREFilter to create the QHeader on layer 2
+     * @param vid the vlan id to be set
+     * @param pcp the pcp to be set
+     * @param destMAC the destination MAC to be used
+     * @param destGates the destination gate the layer2 frame shall be forwarded to
+     * @return the destination info
+     */
+    CoRE4INET::IEEE8021QDestinationInfo* createDestinationInfo(uint16_t vid, uint8_t pcp, inet::MACAddress destMAC,
+            std::list<cGate*>& destGates);
+
+    /**
+     * Tries to resolve the destination MAC for the given IPv4 address
+     * @param destAddress the IP address to resolve
+     * @return the MAC address
+     */
+    inet::MACAddress resolveDestMacAddress(inet::IPv4Address destAddress);
 private:
 
 /**
@@ -58,6 +121,31 @@ protected:
      * Local port for UDP socket
      */
     int _localPort;
+
+    /**
+     * VLAN ID for 802.1Q filters in the IPv4oRE network layer
+     */
+    uint16_t _vlanID = -1;
+
+    /**
+     * PCP for 802.1Q filters in the IPv4oRE network layer
+     */
+    uint8_t _pcp = -1;
+
+    /**
+     * Default destination gate for 802.1Q packets in the IPv4oRE network layer
+     */
+    std::string _defaultDestGate = "bgOut[0]";
+
+    /**
+     * The ipv4 module form IPv4oRE stack
+     */
+    CoRE4INET::IPv4oREBase* _networkLayer;
+
+    /**
+     *
+     */
+    inet::IARP* _arp = nullptr;
 private:
 };
 
